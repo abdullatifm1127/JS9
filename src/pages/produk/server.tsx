98543cddc029d9de4306ffd11dac1ffd3cddc11f
latif <mabdullatifm1127@gmail.com>
2026-04-03 22:52:28 +0700

    Bagian 2

--- src/pages/produk/server.tsx
+++ src/pages/produk/server.tsx
@@ -1,12 +1,31 @@
-import TampilanProduk from "@/views/produk";
+import TampilanProduk from "../../views/produk";
 
-const halamanProdukServer = () => {
+
+type ProductType = {
+    id: string;
+    name: string;
+    price: number;
+    image: string;
+    category: string;
+};
+const halamanProdukServer = ({products}: {products: ProductType[]}) => {
     return (
         <div>
             <h1>Halaman Produk Server by Abdul Latif</h1>
-            <TampilanProduk products={[]} />
+            <TampilanProduk products={products} />
         </div>
     );
 }
 
 export default halamanProdukServer;
+
+export async function getServerSideProps() {
+    const res = await fetch("http://localhost:3000/api/products");
+    const respone = await res.json();
+
+    return {
+        props: {
+            products: respone.data,
+        },
+    };
+}
